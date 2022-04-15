2b38bc6743074144c2ee4b234fe43a15c520a02e
Jonathan Phillips <jonathan.scott.phillips@gmail.com>
2022-04-15 01:52:41 -0400

    chore: add ability to specify element to query from
    Add optional element that when
    included queries for given selector from
    there. If not included, then the fixture is
    queried.

--- projects/testing/src/lib/page-elements/page.element.ts
+++ projects/testing/src/lib/page-elements/page.element.ts
@@ -11,10 +11,20 @@ export class PageElement {
      * the specified selector.
      *
      * @param selector The selector used to find the desired element.
+     * @param element Optional element to query from (otherwise query from fixture).
      * @returns The first element that matches given selector or null if no matches found.
      */
-    query<T>(selector: string): T {
-        return this.fixture.nativeElement.querySelector(selector);
+    query<T>(selector: string, element?: HTMLElement): T {
+        if (element) {
+            const res = element.querySelector(selector) as any;
+            if (res) {
+                return res as T;
+            } else {
+                throw Error(`Error searching for ${selector} in ${element.nodeName}`);
+            }
+        } else {
+            return this.fixture.nativeElement.querySelector(selector);
+        }
     }
 
     /**
@@ -22,9 +32,19 @@ export class PageElement {
      * specified selector.
      *
      * @param selector The selector used to find the desired elements.
+     * @param element Optional element to query from (otherwise query from fixture).
      * @returns All elements that match the given selector (empty list if no matches found).
      */
-    queryAll<T>(selector: string): T[] {
-        return this.fixture.nativeElement.querySelectorAll(selector);
+    queryAll<T>(selector: string, element?: HTMLElement): T[] {
+        if (element) {
+            const res = element.querySelectorAll(selector) as any;
+            if (res) {
+                return res as T[];
+            } else {
+                throw Error(`Error searching for ${selector} in ${element.nodeName}`);
+            }
+        } else {
+            return this.fixture.nativeElement.querySelectorAll(selector);
+        }
     }
 }
