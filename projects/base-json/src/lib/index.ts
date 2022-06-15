a7daba1e92be7f43656bcc93b824bba95511b6ae
Jonathan Phillips <jonathan.scott.phillips@gmail.com>
2022-06-15 16:35:12 -0400

    chore: add multiple entryPoints for API documentation

---
+++ projects/base-json/src/lib/index.ts
@@ -0,0 +1,22 @@
+export * from './constants/symbols';
+
+export * from './converters/date/date.converter';
+
+export * from './decorators/attribute.decorator';
+export * from './decorators/json-api-datastore-config.decorator';
+export * from './decorators/json-api-model-config.decorator';
+
+export * from './interfaces/attribute-decorator-options.interface';
+export * from './interfaces/datastore-config.interface';
+export * from './interfaces/json-api-error.interface';
+export * from './interfaces/model-config.interface';
+export * from './interfaces/model-type.interface';
+export * from './interfaces/overrides.interface';
+export * from './interfaces/property-converter.interface';
+
+export * from './models/error-response.model';
+export * from './models/json-api-meta.model';
+export * from './models/json-api-query-data';
+export * from './models/json.model';
+
+export * from './services/json-datastore.service';
