8e51bbfdcea59acd6a07cfaa5524051f16e7ee9e
Jonathan Phillips <jonathan.scott.phillips@gmail.com>
2022-06-15 16:31:00 -0400

    chore: add typedoc for generating API documentation

---
+++ projects/widgets/src/lib/index.ts
@@ -0,0 +1,29 @@
+export * from './dialog/components/confirm-delete-dialog/confirm-delete-dialog.component';
+export * from './dialog/dialog.module';
+
+export * from './form/directives/click-stop-propagation.directive';
+export * from './form/directives/tab-stop-propagation.directive';
+export * from './form/interfaces/validation-message.interface';
+export * from './form/interfaces/validation-messages.interface';
+export * from './form/pages/field-error/field-error.component';
+export * from './form/form.module';
+
+export * from './grid/pages/paged-grid/paged-grid.component';
+export * from './grid/grid.module';
+
+export * from './layout/interfaces/sidenav.interface';
+export * from './layout/services/loading.service';
+export * from './layout/pages/default-layout/default-layout.component';
+export * from './layout/layout.module';
+
+export * from './list/pages/paged-list/paged-list.component';
+export * from './list/list.module';
+
+export * from './services/remote-data-source.service';
+
+export * from './table/components/filter-drop-down/filter-drop-down.component';
+export * from './table/interfaces/paged-table-with-toolbar.interface';
+export * from './table/pages/abstract-paged-table-with-toolbar/abstract-paged-table-with-toolbar.component';
+export * from './table/pages/paged-table/paged-table.component';
+export * from './table/pages/paged-table-with-toolbar/paged-table-with-toolbar.component';
+export * from './table/table.module';
--- projects/widgets/src/public-api.ts
+++ projects/widgets/src/public-api.ts
@@ -1,32 +1,4 @@
 /*
  * Public API Surface of widgets
  */
-export * from './lib/dialog/components/confirm-delete-dialog/confirm-delete-dialog.component';
-export * from './lib/dialog/dialog.module';
-
-export * from './lib/form/directives/click-stop-propagation.directive';
-export * from './lib/form/directives/tab-stop-propagation.directive';
-export * from './lib/form/interfaces/validation-message.interface';
-export * from './lib/form/interfaces/validation-messages.interface';
-export * from './lib/form/pages/field-error/field-error.component';
-export * from './lib/form/form.module';
-
-export * from './lib/grid/pages/paged-grid/paged-grid.component';
-export * from './lib/grid/grid.module';
-
-export * from './lib/layout/interfaces/sidenav.interface';
-export * from './lib/layout/services/loading.service';
-export * from './lib/layout/pages/default-layout/default-layout.component';
-export * from './lib/layout/layout.module';
-
-export * from './lib/list/pages/paged-list/paged-list.component';
-export * from './lib/list/list.module';
-
-export * from './lib/services/remote-data-source.service';
-
-export * from './lib/table/components/filter-drop-down/filter-drop-down.component';
-export * from './lib/table/interfaces/paged-table-with-toolbar.interface';
-export * from './lib/table/pages/abstract-paged-table-with-toolbar/abstract-paged-table-with-toolbar.component';
-export * from './lib/table/pages/paged-table/paged-table.component';
-export * from './lib/table/pages/paged-table-with-toolbar/paged-table-with-toolbar.component';
-export * from './lib/table/table.module';
+export * from './lib';
