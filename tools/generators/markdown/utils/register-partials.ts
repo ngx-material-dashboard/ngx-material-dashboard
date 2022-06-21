188a6a2363cbd2b59d3d5baa95e4a79d0625fdb4
Jonathan Phillips <jonathan.scott.phillips@gmail.com>
2022-06-20 23:58:29 -0400

    chore: remove console.log statement

--- tools/generators/markdown/utils/register-partials.ts
+++ tools/generators/markdown/utils/register-partials.ts
@@ -5,7 +5,6 @@ import * as path from 'path';
 export function registerPartials() {
     const partialsFolder = path.join(__dirname, '..', 'partials');
     const partialFiles = fs.readdirSync(partialsFolder);
-    console.log(partialFiles);
     partialFiles.forEach((partialFile) => {
         const partialName = path.basename(partialFile, '.hbs');
         const partialContent = fs
