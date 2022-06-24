ffb0fad3e74e77f2cfa5f9a12c40ba30efa05921
Jonathan Phillips <jonathan.scott.phillips@gmail.com>
2022-06-23 23:43:49 -0400

    fix: compilation error

--- projects/documentation/src/app/app.component.spec.ts
+++ projects/documentation/src/app/app.component.spec.ts
@@ -23,7 +23,6 @@ describe('AppComponent', () => {
   it(`should have as title 'documentation'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('documentation');
   });
 
   it('should render title', () => {
