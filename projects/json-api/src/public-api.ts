825aa010d27a0c0829655802fd025db38031a154
Jonathan Phillips <jonathan.scott.phillips@gmail.com>
2022-06-29 21:20:34 -0400

    chore: add more libraries to typdoc
    Add json-api and testing libraries to API documentation.

--- projects/json-api/src/public-api.ts
+++ projects/json-api/src/public-api.ts
@@ -1,12 +1,4 @@
 /*
  * Public API Surface of json-api
  */
-export * from './lib/decorators/belongs-to.decorator';
-export * from './lib/decorators/has-many.decorator';
-export * from './lib/decorators/json-attribute.decorator';
-export * from './lib/decorators/nested-attribute.decorator';
-
-export * from './lib/models/json-api.model';
-export * from './lib/models/json-nested.model';
-
-export * from './lib/services/json-api-datastore.service';
+export * from './lib';
--- projects/testing/src/public-api.ts
+++ projects/testing/src/public-api.ts
@@ -1,19 +1,4 @@
 /*
  * Public API Surface of testing
  */
-
-export * from './lib/fixtures/dummy-object.fixture';
-
-export * from './lib/mocks/datastore.service';
-export * from './lib/mocks/dummy-object.mock';
-
-export * from './lib/page-elements/button/button.element';
-export * from './lib/page-elements/checkbox/checkbox.element';
-export * from './lib/page-elements/page/page.element';
-export * from './lib/page-elements/sidenav/sidenav.element';
-export * from './lib/page-elements/paged-table/paged-table.element';
-export * from './lib/page-elements/paged-table-with-toolbar/paged-table-with-toolbar.element';
-export * from './lib/page-elements/toolbar-header/toolbar-header.element';
-export * from './lib/page-elements/toolbar/toolbar.element';
-
-export * from './lib/page-objects/default-layout.page';
+export * from './lib';
