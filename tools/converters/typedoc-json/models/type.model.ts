4147ddfa3ad04f7d86db4adf49a5b852f146d513
Jonathan Phillips <jonathan.scott.phillips@gmail.com>
2022-07-01 17:00:42 -0400

    fix: undefined for type arguments
    Update typeAgruments property to match format in typedoc.json, which is an array.

--- tools/converters/typedoc-json/models/type.model.ts
+++ tools/converters/typedoc-json/models/type.model.ts
@@ -5,7 +5,7 @@ export class TypeModel {
     /** Defines whether this type is an array or not. */
     type!: string;
     /** Any included type arguments (i.e. "<T>" for type). */
-    typeArguments?: { type: string, id: number, name: string };
+    typeArguments?: { type: string, id: number, name: string }[];
     /** 
      * Type information for arrays (there may be more this includes, but that's
      * what I've determined so far).
@@ -24,7 +24,8 @@ export class TypeModel {
             // contain type of array
             type = `${this.elementType?.name}`;
             if (this.typeArguments) {
-                type += `<${this.typeArguments.name}>`;
+
+                type += `<${this.typeArguments[0].name}>`;
             }
 
             type += '[]';
@@ -33,7 +34,7 @@ export class TypeModel {
             // name of property type whether they are primitive or Objects
             type = this.name;
             if (this.typeArguments) {
-                type += `<${this.typeArguments.name}>`;
+                type += `<${this.typeArguments[0].name}>`;
             }
         }
 
