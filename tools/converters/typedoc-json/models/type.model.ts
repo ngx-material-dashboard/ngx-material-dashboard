9c7307986e281ea0654cb4863aaa6501064b6cf9
Jonathan Phillips <jonathan.scott.phillips@gmail.com>
2022-07-01 17:28:44 -0400

    fix: rendering display type data for unions
    Add types array which contains more TypeModel information for all possible types
    defined when type === 'union', and update displayType to include case for type
    'union' to render '|' between possible types.

--- tools/converters/typedoc-json/models/type.model.ts
+++ tools/converters/typedoc-json/models/type.model.ts
@@ -4,6 +4,7 @@ export class TypeModel {
     name!: string;
     /** Defines whether this type is an array or not. */
     type!: string;
+    types?: TypeModel[];
     /** Any included type arguments (i.e. "<T>" for type). */
     typeArguments?: { type: string, id: number, name: string }[];
     /** 
@@ -14,21 +15,36 @@ export class TypeModel {
 
     constructor(data: Partial<TypeModel>) {
         Object.assign(this, data);
+        if (data.type === 'union') {
+            this.types = [];
+            data.types?.forEach((t: Partial<TypeModel>) => {
+                this.types?.push(new TypeModel(t));
+            });
+        }
+
         this.displayType = this.getDisplayType();
     }
 
     private getDisplayType(): string {
-        let type;
+        let type: string;
         if (this.type === 'array') {
             // if the type is array, then elementType.name should
             // contain type of array
             type = `${this.elementType?.name}`;
             if (this.typeArguments) {
-
                 type += `<${this.typeArguments[0].name}>`;
             }
 
             type += '[]';
+        } else if (this.type === 'union') {
+            type = '';
+            this.types?.forEach((t: TypeModel, index: number) => {
+                if (index > 0) {
+                    type += ' | ';
+                }
+
+                type += t.displayType;
+            });
         } else {
             // if type is not an array, then name should contain
             // name of property type whether they are primitive or Objects
