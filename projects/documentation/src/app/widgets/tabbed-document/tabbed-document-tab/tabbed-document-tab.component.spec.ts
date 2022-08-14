9768cb11b10b000732e9d8c64f33fb176e3a6b75
Jonathan Phillips <jonathan.scott.phillips@gmail.com>
2022-08-14 14:19:52 -0400

    fix(karma): failing test
    Add missing provider for AnchorService.

--- projects/documentation/src/app/widgets/tabbed-document/tabbed-document-tab/tabbed-document-tab.component.spec.ts
+++ projects/documentation/src/app/widgets/tabbed-document/tabbed-document-tab/tabbed-document-tab.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { AnchorService } from '../../../shared/anchor/anchor.service';
 
 import { TabbedDocumentTabComponent } from './tabbed-document-tab.component';
 
@@ -10,7 +11,8 @@ describe('TabbedDocumentTabComponent', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
             declarations: [ TabbedDocumentTabComponent ],
-            imports: [RouterTestingModule]
+            imports: [RouterTestingModule],
+            providers: [AnchorService]
         });
     });
 
