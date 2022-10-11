a15803de3efdd83a841c5622d4f617ea8b72240f
Jonathan Phillips <jonathan.scott.phillips@gmail.com>
2022-10-11 17:26:57 -0400

    chore: add button click actions enum

---
+++ projects/widgets/src/lib/shared/enums/button-click-actions.enum.ts
@@ -0,0 +1,10 @@
+/**
+ * The `ButtonClickAction` enum defines the default actions available for
+ * buttons related to collections. The basic actions are Create, Edit, and
+ * Delete.
+ */
+export enum ButtonClickAction {
+    Create = 'create',
+    Edit = 'edit',
+    Delete = 'delete'
+};
