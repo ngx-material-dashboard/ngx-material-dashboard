8139d17b06895e473ad0fdb8570df2cdce64bac5
Jonathan Phillips <jonathan.scott.phillips@gmail.com>
2022-12-22 01:16:29 -0500

    fix: lint warning

--- projects/widgets/src/lib/list/pages/paged-list/paged-list.component.sandbox.ts
+++ projects/widgets/src/lib/list/pages/paged-list/paged-list.component.sandbox.ts
@@ -10,10 +10,7 @@ import { MatSortModule } from '@angular/material/sort';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { JsonDatastore } from '@ngx-material-dashboard/base-json';
-import {
-    Datastore,
-    getTaskData
-} from '@ngx-material-dashboard/testing';
+import { Datastore, getTaskData } from '@ngx-material-dashboard/testing';
 import { sandboxOf } from 'angular-playground';
 import { CollectionModule } from '../../../collection/collection.module';
 import { DEFAULT_COLLECTION_BUTTONS } from '../../../collection/shared/buttons';
