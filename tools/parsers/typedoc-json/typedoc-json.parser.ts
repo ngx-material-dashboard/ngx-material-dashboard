2debd9d0d9078ece790f9d1e11f9fd819501597b
Jonathan Phillips <jonathan.scott.phillips@gmail.com>
2023-02-21 01:13:02 -0500

    chore: add override to remove package info from ReferenceTypeParser

--- tools/parsers/typedoc-json/typedoc-json.parser.ts
+++ tools/parsers/typedoc-json/typedoc-json.parser.ts
@@ -1,4 +1,6 @@
+import { ReferenceTypeParser, TypeParser } from 'typedoc-json-parser';
 import { FileUtil } from '../../util/file.util';
+import { ProjectParser } from './parsers';
 import { WorkspaceParser } from './parsers/workspace/workspace.parser';
 
 /**
@@ -12,6 +14,23 @@ export class TypedocJsonParser {
     constructor(fileName: string) {
         const str = FileUtil.read(__dirname, '..', '..', '..', fileName);
         const data = JSON.parse(str);
+        this.initOverrides();
         this.workspaceParser = new WorkspaceParser({ data });
+    }
+
+    private initOverrides() {
+        ReferenceTypeParser.formatToString = (
+            options: TypeParser.FormatToStringOptions<ReferenceTypeParser>
+        ) => {
+            const { parser, project } = options;
+            const typeArguments =
+                parser.typeArguments.length > 0
+                    ? `<${parser.typeArguments
+                          .map((type) => type.toString(project))
+                          .join(', ')}>`
+                    : '';
+
+            return `${parser.name}${typeArguments}`;
+        };
     }
 }
