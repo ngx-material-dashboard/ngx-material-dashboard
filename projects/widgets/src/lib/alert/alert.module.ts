27a69159a048997e94af0e1c3b1b247e3c543eac
Jonathan Phillips <13894312+jphillips03@users.noreply.github.com>
2023-04-14 01:04:56 -0400

    fix: alert layout
    Add flex layout module to alert module to fix layout
    issue where close button is rendered next to alert
    message instead of on right hand side of alert.

--- projects/widgets/src/lib/alert/alert.module.ts
+++ projects/widgets/src/lib/alert/alert.module.ts
@@ -14,10 +14,17 @@ import { MatButtonModule } from '@angular/material/button';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { AlertsComponent } from './components/alerts/alerts.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
     declarations: [AlertComponent, AlertsComponent],
     exports: [AlertComponent],
-    imports: [CommonModule, MatButtonModule, OverlayModule, FontAwesomeModule]
+    imports: [
+        CommonModule,
+        FlexLayoutModule,
+        MatButtonModule,
+        OverlayModule,
+        FontAwesomeModule
+    ]
 })
 export class AlertModule {}
